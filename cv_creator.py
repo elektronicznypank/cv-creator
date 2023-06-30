987ee7408d60c373b656d8c652fb3b9e0807c06e
elektronicznypank <dizelpauer115@gmail.com>
2023-06-30 22:42:30 +0200

    Variable name change

--- cv_creator.py
+++ cv_creator.py
@@ -1,6 +1,6 @@
 import json
 
 with open('data.json', encoding='utf-8') as json_file:
-    json_dict = json.load(json_file)
+    data_from_json = json.load(json_file)
 
-print(json_dict)
+print(data_from_json)
